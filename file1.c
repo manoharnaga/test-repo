e2a677902baf3a393149c07874dc144cf1083af2
manoharnaga <manoharnaga333@gmail.com>
2022-04-01 23:10:45 +0530

    file1.c is added to the repo:)

---
+++ file1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+int main(){
+    int n = 10;
+    for(char ch='a';ch<='z';ch++){
+        printf("%c\t%d\t%x",ch,ch,ch);
+    }
+    printf("\n");
+    printf("Hello new repo about to be made \n");
+    printf("Get read folks!\n");
+    
+    return 0;
+}
